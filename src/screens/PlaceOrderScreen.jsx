5dd47ac7268dbfb0f7d9eaba2157091ace410794
Mehran096 <mehranh91309@gmail.com>
2026-05-17 12:46:36 +0500

    something new adde =

--- src/screens/PlaceOrderScreen.jsx
+++ src/screens/PlaceOrderScreen.jsx
@@ -66,6 +66,14 @@ const PlaceOrderScreen = () => {
     )
   }
 
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-64'>
+        <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900'></div>
+      </div>
+    )
+  }
+
   return (
     <div className="container mx-auto px-4 py-8">
       <CheckoutSteps step1 step2 step3 step4 />
